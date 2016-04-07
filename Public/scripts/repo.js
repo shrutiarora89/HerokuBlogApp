f6d5dac35c5ea3ad82e3d38a3654519917020ed7
Shruti Arora <shruti.arora89@icloud.com>
2016-04-07 08:29:38 -0700

    updating ajax url

--- Public/scripts/repo.js
+++ Public/scripts/repo.js
@@ -7,7 +7,7 @@
   // to the proxy end point provided by server.js.
   repos.requestRepos = function(callback) {
     $.ajax({
-      url: 'http://api.github.com/user/repos'+
+      url: 'https://api.github.com/user/repos'+
            '?per_page=100' +
             '&sort=updated',
       type: 'GET',
